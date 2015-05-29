87c0c24d87209e195ff887e86b63d2f2e1f120b5
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-05-29 08:02:23 +0200

    WIP on migration - missing hystrix, aspectj, issues with guava

---
+++ spring-cloud-sleuth-zipkin/src/main/java/org/springframework/cloud/sleuth/zipkin/web/correlationId/EnableCorrelationId.java
@@ -0,0 +1,20 @@
+package org.springframework.cloud.sleuth.zipkin.web.correlationId;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Enables support for correlation id.
+ *
+ * @see com.ofg.infrastructure.web.correlationid.CorrelationIdConfiguration
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(CorrelationIdConfiguration.class)
+public @interface EnableCorrelationId {
+
+}
---
+++ spring-cloud-sleuth-zipkin/src/main/java/org/springframework/cloud/sleuth/zipkin/web/correlationId/UuidGenerator.java
@@ -0,0 +1,10 @@
+package org.springframework.cloud.sleuth.zipkin.web.correlationId;
+
+import java.util.UUID;
+
+public class UuidGenerator {
+    public String create() {
+        return UUID.randomUUID().toString();
+    }
+
+}
