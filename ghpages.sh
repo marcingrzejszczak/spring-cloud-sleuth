6ddd6dafe74346772959025f73a98caed35176a1
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-08-18 11:15:57 +0200

    Sync docs from 1.0.x to gh-pages

--- ghpages.sh
+++ ghpages.sh
@@ -22,29 +22,22 @@ if [[ ! -e "${ROOT_FOLDER}/.git" ]]; then
     exit 1
 fi
 
-# Retrieve properties
+# Retrieve version number, name of the main adoc and name of the current branch
 ###################################################################
 
-# Prop that will let commit the changes
-COMMIT_CHANGES="no"
+# Code grepping for the 1st presence of "version>" in pom.xml.
+# First one is project version, second parent version.
+VERSION_NODE=`awk '/version>/{i++}i==1{print; exit}' $ROOT_FOLDER/pom.xml`
+# Extract the contents of the version node
+VERSION_VALUE=$(sed -ne '/version/{s/.*<version>\(.*\)<\/version>.*/\1/p;q;}' <<< "$VERSION_NODE")
+echo "Extracted version from root pom.xml is [${VERSION_VALUE}]"
 
-# Get the name of the `docs.main` property
-MAIN_ADOC_VALUE=$(mvn -q \
-    -Dexec.executable="echo" \
-    -Dexec.args='${docs.main}' \
-    --non-recursive \
-    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-echo "Extracted 'main.adoc' from Maven build [${MAIN_ADOC_VALUE}]"
-
-# Get whitelisted branches - assumes that a `docs` module is available under `docs` profile
-WHITELIST_PROPERTY="docs.whitelisted.branches"
-WHITELISTED_BRANCHES_VALUE=$(mvn -q \
-    -Dexec.executable="echo" \
-    -Dexec.args="\${${WHITELIST_PROPERTY}}" \
-    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec \
-    -P docs \
-    -pl docs)
-echo "Extracted '${WHITELIST_PROPERTY}' from Maven build [${WHITELISTED_BRANCHES_VALUE}]"
+# Code grepping for the 2nd presence of "version>" in pom.xml.
+# First one is parent, second project version.
+MAIN_ADOC_NODE=`awk '/docs.main/{i++}i==1{print; exit}' $ROOT_FOLDER/docs/pom.xml`
+# Extract the contents of the version node
+MAIN_ADOC_VALUE=$(sed -ne '/docs.main/{s/.*<docs.main>\(.*\)<\/docs.main>.*/\1/p;q;}' <<< "$MAIN_ADOC_NODE")
+echo "Extracted version from docs pom.xml is [${MAIN_ADOC_VALUE}]"
 
 # Code getting the name of the current branch. For master we want to publish as we did until now
 # http://stackoverflow.com/questions/1593051/how-to-programmatically-determine-the-current-checked-out-git-branch
@@ -66,55 +59,47 @@ git pull origin gh-pages
 
 # Add git branches
 ###################################################################
-mkdir -p ${ROOT_FOLDER}/${CURRENT_BRANCH}
+mkdir -p ${ROOT_FOLDER}/${VERSION_VALUE}
 if [[ "${CURRENT_BRANCH}" == "master" ]] ; then
-    echo -e "Current branch is master - will copy the current docs only to the root folder"
+    echo -e "Current branch is master - will copy the current docs also to [${VERSION_VALUE}] folder"
     for f in docs/target/generated-docs/*; do
         file=${f#docs/target/generated-docs/*}
         if ! git ls-files -i -o --exclude-standard --directory | grep -q ^$file$; then
             # Not ignored...
             cp -rf $f ${ROOT_FOLDER}/
-            git add -A ${ROOT_FOLDER}/$file
+            cp -rf $f ${ROOT_FOLDER}/${VERSION_VALUE}
+            # We want users to access 1.0.0.RELEASE/ instead of 1.0.0.RELEASE/spring-cloud.sleuth.html
+            if [[ "${file}" == "${MAIN_ADOC_VALUE}" ]] ; then
+                ln -s ${ROOT_FOLDER}/${VERSION_VALUE}/index.html ${ROOT_FOLDER}/${VERSION_VALUE}/${MAIN_ADOC_VALUE}.adoc
+                git add -A ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
+            fi
+            git add -A $file
+            git add -A ${ROOT_FOLDER}/${VERSION_VALUE}/$file
         fi
     done
-    COMMIT_CHANGES="yes"
 else
-    echo -e "Current branch is [${CURRENT_BRANCH}]"
-    # http://stackoverflow.com/questions/29300806/a-bash-script-to-check-if-a-string-is-present-in-a-comma-separated-list-of-strin
-    if [[ ",${WHITELISTED_BRANCHES_VALUE}," = *",${CURRENT_BRANCH},"* ]] ; then
-        echo -e "Branch [${CURRENT_BRANCH}] is whitelisted! Will copy the current docs to the [${CURRENT_BRANCH}] folder"
-        for f in docs/target/generated-docs/*; do
-            file=${f#docs/target/generated-docs/*}
-            if ! git ls-files -i -o --exclude-standard --directory | grep -q ^$file$; then
-                # Not ignored...
-                # We want users to access 1.0.0.RELEASE/ instead of 1.0.0.RELEASE/spring-cloud.sleuth.html
-                if [[ "${file}" == "${MAIN_ADOC_VALUE}.html" ]] ; then
-                    # We don't want to copy the spring-cloud-sleuth.html
-                    # we want it to be converted to index.html
-                    cp -rf $f ${ROOT_FOLDER}/${CURRENT_BRANCH}/index.html
-                    git add -A ${ROOT_FOLDER}/${CURRENT_BRANCH}/index.html
-                else
-                    cp -rf $f ${ROOT_FOLDER}/${CURRENT_BRANCH}
-                    git add -A ${ROOT_FOLDER}/${CURRENT_BRANCH}/$file
-                fi
+    echo -e "Current branch is [${CURRENT_BRANCH}] - will copy the current docs ONLY to [${VERSION_VALUE}] folder"
+    for f in docs/target/generated-docs/*; do
+        file=${f#docs/target/generated-docs/*}
+        if ! git ls-files -i -o --exclude-standard --directory | grep -q ^$file$; then
+            # Not ignored...
+            cp -rf $f ${ROOT_FOLDER}/${VERSION_VALUE}
+            if [[ "${file}" == "${MAIN_ADOC_VALUE}" ]] ; then
+                ln -s ${ROOT_FOLDER}/${VERSION_VALUE}/index.html ${ROOT_FOLDER}/${VERSION_VALUE}/${MAIN_ADOC_VALUE}.adoc
+                git add -A ${ROOT_FOLDER}/${VERSION_VALUE}/index.html
             fi
-        done
-        COMMIT_CHANGES="yes"
-    else
-        echo -e "Branch [${CURRENT_BRANCH}] is not on the white list! Check out the Maven [${WHITELIST_PROPERTY}] property in
-         [docs] module available under [docs] profile. Won't commit any changes to gh-pages for this branch."
-    fi
+            git add -A ${ROOT_FOLDER}/${VERSION_VALUE}/$file
+        fi
+    done
 fi
 
-if [[ "${COMMIT_CHANGES}" == "yes" ]] ; then
-    git commit -a -m "Sync docs from ${CURRENT_BRANCH} to gh-pages"
+git commit -a -m "Sync docs from ${CURRENT_BRANCH} to gh-pages"
 
-    # Uncomment the following push if you want to auto push to
-    # the gh-pages branch whenever you commit to master locally.
-    # This is a little extreme. Use with care!
-    ###################################################################
-    git push origin gh-pages
-fi
+# Uncomment the following push if you want to auto push to
+# the gh-pages branch whenever you commit to master locally.
+# This is a little extreme. Use with care!
+###################################################################
+git push origin gh-pages
 
 # Finally, switch back to the master branch and exit block
 git checkout ${CURRENT_BRANCH}
